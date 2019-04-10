6b93d491fb03a8227ea063e81b384e9d22575610
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-10 12:39:40 -0700

    add_cubes and add_mult functions built and working in calculator

--- arithmetic.py
+++ arithmetic.py
@@ -34,4 +34,11 @@ def power(num1, num2):
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
     return num1 % num2
-    
+
+def add_mult(num1, num2, num3):
+    """Adds num1 and num2 and then multiplies that sum by num3"""
+    return (num1 + num2) * num3
+
+def add_cubes(num1, num2):
+    """Cubes num1 and num2 returns sum of the cubes"""
+    return num1 ** 3 + num2 ** 3
